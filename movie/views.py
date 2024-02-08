ec23287c904a485eafc3ae9b25f09fe64333103c
Juan José Restrepo Higuita <jjrestre10@eafit.edu.co>
2024-02-08 11:11:38 -0500

    cambios en la función home

--- movie/views.py
+++ movie/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to home page</h1>')
     #return render(request,'home.html')
-    return render(request,'home.html',{'name':'Juan José Restrepo Higuita'})
+    return render(request,'home.html',{'name':'Juan Restrepo Higuita'})
 
 def about(request):
     return HttpResponse('<h1>About</h1>')
